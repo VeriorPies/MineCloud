4912b396ebb0e0de52a6be5a21ebb8c7e7d4f492
314pies <ian121363@gmail.com>
2023-04-02 20:51:09 -0700

    Add CDK code to EC2 spot instance

--- lib/m_cloud-stack.ts
+++ lib/m_cloud-stack.ts
@@ -1,16 +1,76 @@
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { SpotInstance } from './spot_instance';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { AmazonLinuxGeneration, AmazonLinuxImage, CfnKeyPair, InstanceClass, InstanceSize, InstanceType, Peer, Port, SecurityGroup, SpotInstanceInterruption, SpotRequestType, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 
-export class MCloudStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+const STACK_PREFIX = 'MCloud';
+const MAX_PRICE = 0.1; // EC2 max price
+const EC2_INSTANCE_TYPE = InstanceType.of(
+  InstanceClass.T2,
+  InstanceSize.LARGE
+);
+
+export class MCloudStack extends Stack {
+
+  readonly ec2Instance;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    this.ec2Instance = this.setupEC2Instance();
+  }
 
-    // The code that defines your stack goes here
+  setupEC2Instance(): SpotInstance {
+    const vpc = new Vpc(this, `${STACK_PREFIX}_VPC`);
+    
+    const ec2Role = new Role(
+      this,
+      `${STACK_PREFIX}_ec2_instance_role`,
+      { assumedBy: new ServicePrincipal('ec2.amazonaws.com') }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MCloudQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const securityGroup = new SecurityGroup(
+      this,
+      `${STACK_PREFIX}_ec2_security_group`,
+      {
+        vpc: vpc,
+        allowAllOutbound: true,
+        securityGroupName: `${STACK_PREFIX}_ec2_security_group`,
+      }
+    )
+    // To allow SSH and minecraft connections
+    securityGroup.addIngressRule(
+      Peer.anyIpv4(),
+      Port.tcp(22),
+      'Allows SSH connection'
+    )
+    securityGroup.addIngressRule(
+      Peer.anyIpv4(),
+      Port.tcp(25565),
+      'Allows Minecraft connection'
+    )
+    
+    // Key pair for ssh-ing into EC2 instance from aws console
+    const sshKeyPair = new CfnKeyPair(this, `${STACK_PREFIX}_ec2_key_pair`, {
+      keyName: `${STACK_PREFIX}_ec2_key`});
+      
+    return new SpotInstance(this, `${STACK_PREFIX}_ec2_instance`, {
+      vpc: vpc,
+      keyName: sshKeyPair.keyName,
+      role: ec2Role,
+      vpcSubnets: {
+        // Place in a public subnet in-order to have a public ip address
+        subnetType: SubnetType.PUBLIC
+      },
+      securityGroup: securityGroup,
+      instanceType: EC2_INSTANCE_TYPE,
+      machineImage: new AmazonLinuxImage({generation: AmazonLinuxGeneration.AMAZON_LINUX_2}),
+      templateId: `${STACK_PREFIX}_ec2_launch_template`,
+      launchTemplateSpotOptions:{
+        interruptionBehavior: SpotInstanceInterruption.STOP,
+        requestType: SpotRequestType.PERSISTENT,
+        maxPrice: MAX_PRICE,
+      },
+    });
   }
 }
---
+++ lib/spot_instance.ts
@@ -0,0 +1,24 @@
+import type { InstanceProps, LaunchTemplateSpotOptions } from "aws-cdk-lib/aws-ec2";
+import { Instance, LaunchTemplate, SpotRequestType } from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
+
+
+export interface SpotInstanceProps extends InstanceProps {
+    readonly templateId: string;
+    readonly launchTemplateSpotOptions: LaunchTemplateSpotOptions;
+}
+
+export class SpotInstance extends Instance {
+    public constructor(scope: Construct, id: string, props: SpotInstanceProps) {
+        super(scope, id, props);
+
+        // Make this a spot instance
+        const template = new LaunchTemplate(this, props.templateId, {
+            spotOptions: props.launchTemplateSpotOptions
+        });
+        this.instance.launchTemplate = {
+            version: template.versionNumber,
+            launchTemplateId: template.launchTemplateId,
+        };
+    }
+}
