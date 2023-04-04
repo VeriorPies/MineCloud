aa52a49b92c22a50a22437ca420daf9a2b817bae
314pies <ian121363@gmail.com>
2023-04-03 19:49:36 -0700

    Rename project name from m_cloud to mine-cloud

--- test/mine-cloud.test.ts
+++ test/mine-cloud.test.ts
@@ -1,9 +1,9 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as MineCloud from '../lib/m_cloud-stack';
+// import * as MineCloud from '../lib/mine-cloud-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/m_cloud-stack.ts
+// example resource in lib/mine-cloud-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
