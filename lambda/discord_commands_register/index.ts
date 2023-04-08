d8fcfa5866473db4d222442b9d3364d58e91c5b9
314pies <ian121363@gmail.com>
2023-04-08 02:54:35 -0700

    Setup Discord BOT commands during EC2 initialization

---
+++ lambda/discord_commands_register/index.ts
@@ -0,0 +1,52 @@
+import {
+    CdkCustomResourceEvent,
+    CdkCustomResourceResponse,
+    Context,
+  } from 'aws-lambda';
+import axios from 'axios';
+
+exports.handler = async (event: CdkCustomResourceEvent,
+    context: Context,) => {
+    
+    if (event.RequestType !== 'Delete') {
+        const apiEndpoint = `https://discord.com/api/v10/applications/${process.env.APP_ID}/commands`;
+        await registerCommand("mc_start", "Start the minecraft server", apiEndpoint);
+        await registerCommand("mc_stop", "Start the minecraft server", apiEndpoint);
+        await registerCommand("mc_backup", "Start the minecraft server", apiEndpoint);
+        await registerCommand("mc_restart", "Start the minecraft server", apiEndpoint);
+        console.log("Discord command register completed");
+    }
+   
+
+    const response: CdkCustomResourceResponse = {
+        StackId: event.StackId,
+        RequestId: event.RequestId,
+        LogicalResourceId: event.LogicalResourceId,
+        PhysicalResourceId: context.logGroupName,
+    };
+
+    response.Status = 'SUCCESS';
+    response.Data = { Result: 'None' };
+    return response;
+}
+
+async function registerCommand(commandName: string, description: string, endPoint: string) {
+    const header = {
+        "Authorization": `Bot ${process.env.BOT_TOKEN}`
+    }
+    const body = {
+        "name": commandName,
+        "type": 1,
+        "description": description,
+    }
+    const request = {
+        method: 'post',
+        url: endPoint,
+        headers: header,
+        data: body
+    };
+
+    console.log('register request: ', request);
+    const response = await axios(request);
+    console.log('register response: ', response.data);
+}
--- lib/mine-cloud-stack.ts
+++ lib/mine-cloud-stack.ts
@@ -1,6 +1,6 @@
 import { Construct } from "constructs";
 import { SpotInstance } from "./spot-instance";
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, CustomResource, Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 import {
   AmazonLinuxGeneration,
@@ -28,7 +28,14 @@ import {
   Vpc,
 } from "aws-cdk-lib/aws-ec2";
 import { DiscordInteractionsEndpointConstruct } from './discord-interactions-endpoint-construct';
+import * as cr from 'aws-cdk-lib/custom-resources'
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import path = require("path");
 
+const DISCORD_PUBLIC_KEY = "";
+const DISCORD_APP_ID = "";
+const DISCORD_BOT_TOKEN = "";
 
 export const STACK_PREFIX = 'MineCloud';
 
@@ -37,7 +44,6 @@ const EC2_INSTANCE_TYPE = InstanceType.of(
   InstanceClass.T2,
   InstanceSize.LARGE
 );
-const DISCORD_PUBLIC_KEY = "";
 
 const MINECRAFT_USER = "minecraft";
 // Not the same name since cfn-init can't figure it out for some reason
@@ -45,14 +51,22 @@ const MINECRAFT_GROUP = "minecraft-group";
 const MINECRAFT_BASE_DIR = "/opt/minecraft";
 const MINECRAFT_SERVER_DIR = `${MINECRAFT_BASE_DIR}/server`;
 
+
+
 export class MineCloud extends Stack {
 
   readonly ec2Instance;
-  readonly  discordInteractionsEndpointLambda;
+  
+  readonly discordInteractionsEndpointLambda;
+  readonly discordInteractionsEndpointURL: CfnOutput;
+  
+  readonly discordCommandRegisterResource: CustomResource;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     this.ec2Instance = this.setupEC2Instance();
+    this.discordCommandRegisterResource = this.setupDiscordCommands();
+
     this.discordInteractionsEndpointLambda = new DiscordInteractionsEndpointConstruct(
       this,  
       `${STACK_PREFIX}_discord_interactions_endpoint_construct`,
@@ -63,6 +77,8 @@ export class MineCloud extends Stack {
       }
     );
     this.discordInteractionsEndpointLambda.node.addDependency(this.ec2Instance);
+    this.discordInteractionsEndpointURL = new CfnOutput(this, 'DISCORD-INTERACTIONS-ENDPOINT-URL', { 
+      value: this.discordInteractionsEndpointLambda.lambdaFunctionURL.url });
   }
 
   setupEC2Instance(): SpotInstance {
@@ -198,5 +214,34 @@ export class MineCloud extends Stack {
         ]),
       },
     });
+  }
+
+
+  setupDiscordCommands(): CustomResource{
+    const role = new Role(
+      this,
+      `${STACK_PREFIX}_discord_command_register_lambda_role`,
+      { assumedBy: new ServicePrincipal('lambda.amazonaws.com') }
+    );
+
+    const lambda = new NodejsFunction(this, `${STACK_PREFIX}_discord_commands_register_lambda`, {
+      runtime: Runtime.NODEJS_14_X, // We will want to upgrade this later
+      handler: 'index.handler',
+      entry: path.join(__dirname, '/../lambda/discord_commands_register/index.ts'),
+      environment:{
+        APP_ID: DISCORD_APP_ID,
+        BOT_TOKEN: DISCORD_BOT_TOKEN
+      }
+    });
+
+    const provider = new cr.Provider(this, `${STACK_PREFIX}_discord_commands_register_provider`, {
+      onEventHandler: lambda,   
+      role: role, 
+    });
+
+    const customResource = new CustomResource(this, `${STACK_PREFIX}_discord_commands_register_resource`, {
+      serviceToken: provider.serviceToken,
+    });
+    return customResource;
   }
 }
