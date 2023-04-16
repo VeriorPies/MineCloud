46f7b9344428715e976500102739cd63db32249a
314pies <ian121363@gmail.com>
2023-04-15 22:43:21 -0700

    Minor update with the parameter order in minecloud config

--- minecloud_configs/MineCloud-Configs.ts
+++ minecloud_configs/MineCloud-Configs.ts
@@ -2,8 +2,8 @@
 export const AWS_ACCOUNT_NUMBER = '';
 export const AWS_REGION = '';
 
-export const DISCORD_PUBLIC_KEY = '';
 export const DISCORD_APP_ID = '';
+export const DISCORD_PUBLIC_KEY = '';
 export const DISCORD_BOT_TOKEN = '';
 export const DISCORD_CHANNEL_WEB_HOOK = '';
 
