19fd77e9ed7be41bf45451e43e84aaded24853e9
314pies <ian121363@gmail.com>
2023-04-27 19:14:18 -0700

    Change default server setup behavior

--- minecloud_configs/advanced_configs/custom-instance-init.ts
+++ minecloud_configs/advanced_configs/custom-instance-init.ts
@@ -1,6 +1,17 @@
-import { InitConfig, InitPackage } from 'aws-cdk-lib/aws-ec2';
+import { InitCommand, InitConfig, InitFile, InitPackage } from 'aws-cdk-lib/aws-ec2';
+import { MINECLOUD_SERVER_DIR } from '../../lib/const/minecloud-dir';
+
+
+const MINECRAFT_SERVER_DOWNLOAD_URL = 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar';
 
 export const CUSTOM_INIT_CONFIG: InitConfig = new InitConfig([
   // Install an Amazon Linux package using yum
-  InitPackage.yum('java-17-amazon-corretto-headless')
+  InitPackage.yum('java-17-amazon-corretto-headless'),
+  InitCommand.shellCommand(
+    "echo 'eula=true' > eula.txt",
+    {
+      cwd: MINECLOUD_SERVER_DIR,
+    }
+  ),
+  InitFile.fromUrl(`${MINECLOUD_SERVER_DIR}/server.jar`, MINECRAFT_SERVER_DOWNLOAD_URL),
 ]);
