74442c25334cc6823bdb75d7db0ed7f339d7b5a4
314pies <ian121363@gmail.com>
2023-05-13 21:10:46 -0700

    Refactor by packing shell script and env variable setup to it's own function

--- lib/instance-init.ts
+++ lib/instance-init.ts
@@ -23,6 +23,29 @@ const MINECLOUD_USER = 'minecloud';
 // Not the same name since cfn-init can't figure it out for some reason
 const MINECLOUD_GROUP = 'minecloud-group';
 
+
+function setUpShellScript(targetDir: string, targetFileName: string, localFilePath: string){
+  return [
+    InitFile.fromFileInline(
+      `${targetDir}/${targetFileName}`,
+      localFilePath
+    ),
+    InitCommand.shellCommand(`sudo chmod +x ${targetFileName}`, {
+      cwd: targetDir
+    }),
+    // To convert Windows's EOL to Linux
+    InitCommand.shellCommand(`sed -i 's/\r//' ${targetFileName}`, {
+      cwd: targetDir
+    }),
+  ]
+}
+
+function setUpEnviromentVariable(name: string, value: string){
+  return [
+    InitCommand.shellCommand(`echo '${name}=${value}' >> /etc/environment`)
+  ]
+}
+
 export function getInitConfig(backupBucketName: string) {
   return CloudFormationInit.fromConfigSets({
     configSets: {
@@ -64,59 +87,19 @@ export function getInitConfig(backupBucketName: string) {
         InitCommand.shellCommand(`mkdir -p ${MINECLOUD_SERVER_DIR}`),
 
         // Setup server start/stop scripts
-        InitFile.fromFileInline(
-          `${MINECLOUD_SERVER_DIR}/start_server.sh`,
-          'minecloud_configs/server/start_server.sh'
-        ),
-        InitCommand.shellCommand(`sudo chmod +x start_server.sh`, {
-          cwd: MINECLOUD_SERVER_DIR
-        }),
-        InitCommand.shellCommand(`sed -i 's/\r//' start_server.sh`, {
-          cwd: MINECLOUD_SERVER_DIR
-        }),
-
-        InitFile.fromFileInline(
-          `${MINECLOUD_SERVER_DIR}/stop_server.sh`,
-          'minecloud_configs/server/stop_server.sh'
-        ),
-        InitCommand.shellCommand(`sudo chmod +x stop_server.sh`, {
-          cwd: MINECLOUD_SERVER_DIR
-        }),
-        InitCommand.shellCommand(`sed -i 's/\r//' stop_server.sh`, {
-          cwd: MINECLOUD_SERVER_DIR
-        }),
+        ...setUpShellScript(MINECLOUD_SERVER_DIR, 'start_server.sh', 'minecloud_configs/server/start_server.sh'),
+        ...setUpShellScript(MINECLOUD_SERVER_DIR, 'stop_server.sh', 'minecloud_configs/server/stop_server.sh'),
 
         InitCommand.shellCommand(
           `chown -R ${MINECLOUD_USER}:${MINECLOUD_GROUP} ${MINECLOUD_BASE_DIR}`
         )
       ]),
       setupDiscordMessaging: new InitConfig([
-        InitCommand.shellCommand(
-          `echo 'DISCORD_WEB_HOOK=${DISCORD_CHANNEL_WEB_HOOK}' >> /etc/environment`
-        ),
-        InitFile.fromFileInline(
-          `${MINECLOUD_BASE_DIR}/send_discord_message_to_webhook.sh`,
-          'server_init_assets/send_discord_message_to_webhook.sh'
-        ),
-        InitCommand.shellCommand(
-          `sudo chmod +x send_discord_message_to_webhook.sh`,
-          { cwd: MINECLOUD_BASE_DIR }
-        ),
-        InitCommand.shellCommand(`sed -i 's/\r//' send_discord_message_to_webhook.sh`, 
-          { cwd: MINECLOUD_BASE_DIR }
-        ),
+        ...setUpEnviromentVariable('DISCORD_WEB_HOOK', DISCORD_CHANNEL_WEB_HOOK),
+        ...setUpShellScript(MINECLOUD_BASE_DIR, 'send_discord_message_to_webhook.sh', 'server_init_assets/send_discord_message_to_webhook.sh'),
       ]),
       setupMineCloudService: new InitConfig([
-        InitFile.fromFileInline(
-          `${MINECLOUD_BASE_DIR}/start_service.sh`,
-          'server_init_assets/start_service.sh'
-        ),
-        InitCommand.shellCommand(`sudo chmod +x start_service.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
-        InitCommand.shellCommand(`sed -i 's/\r//' start_service.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
+        ...setUpShellScript(MINECLOUD_BASE_DIR, 'start_service.sh', 'server_init_assets/start_service.sh'),
         InitFile.fromFileInline(
           '/etc/systemd/system/minecloud.service',
           'server_init_assets/minecloud.service'
@@ -125,83 +108,25 @@ export function getInitConfig(backupBucketName: string) {
         InitCommand.shellCommand('systemctl start minecloud.service')
       ]),
       setupBackupScripts: new InitConfig([
-        InitCommand.shellCommand(
-          `echo 'BACKUP_BUCKET_NAME=${backupBucketName}' >> /etc/environment`
-        ),
-        InitFile.fromFileInline(
-          `${MINECLOUD_BASE_DIR}/server_backup.sh`,
-          'server_init_assets/server_backup.sh'
-        ),
-        InitCommand.shellCommand(`sudo chmod +x server_backup.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
-        InitCommand.shellCommand(`sed -i 's/\r//' server_backup.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
-        InitCommand.shellCommand(
-          `echo 'MAX_BACKUP_COUNT=${MAX_BACKUP_COUNT}' >> /etc/environment`
-        ),
-
-        InitFile.fromFileInline(
-          `${MINECLOUD_BASE_DIR}/server_manual_backup.sh`,
-          'server_init_assets/server_manual_backup.sh'
-        ),
-        InitCommand.shellCommand(`sudo chmod +x server_manual_backup.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
-        InitCommand.shellCommand(`sed -i 's/\r//' server_manual_backup.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
-
-        InitFile.fromFileInline(
-          `${MINECLOUD_BASE_DIR}/auto_backup_checker.sh`,
-          'server_init_assets/auto_backup_checker.sh'
-        ),
-        InitCommand.shellCommand(`sudo chmod +x auto_backup_checker.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
-        InitCommand.shellCommand(`sed -i 's/\r//' auto_backup_checker.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
-        InitCommand.shellCommand(
-          `echo 'BACKUP_INTERVAL=${BACKUP_INTERVAL_IN_SECONDS}' >> /etc/environment`
-        )
+        ...setUpEnviromentVariable('BACKUP_BUCKET_NAME', backupBucketName),
+        ...setUpShellScript(MINECLOUD_BASE_DIR, 'server_backup.sh', 'server_init_assets/server_backup.sh'),
+        ...setUpEnviromentVariable('MAX_BACKUP_COUNT', MAX_BACKUP_COUNT.toString()),
+        ...setUpShellScript(MINECLOUD_BASE_DIR, 'server_manual_backup.sh', 'server_init_assets/server_manual_backup.sh'),
+        ...setUpShellScript(MINECLOUD_BASE_DIR, 'auto_backup_checker.sh', 'server_init_assets/auto_backup_checker.sh'),
+        ...setUpEnviromentVariable('BACKUP_INTERVAL', BACKUP_INTERVAL_IN_SECONDS.toString()),
       ]),
       setupAutoShutdown: new InitConfig([
         // Setup custom logic for checking how many current connection
-        InitFile.fromFileInline(
-          `${MINECLOUD_BASE_DIR}/get_connection_count.sh`,
-          'minecloud_configs/advanced_configs/get_connection_count.sh'
-        ),
-        InitCommand.shellCommand(`sudo chmod +x get_connection_count.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
-        InitFile.fromFileInline(
-          `${MINECLOUD_BASE_DIR}/check_user_conn.sh`,
-          'server_init_assets/check_user_conn.sh'
-        ),
-        InitCommand.shellCommand(`sudo chmod +x check_user_conn.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
-        InitCommand.shellCommand(`sed -i 's/\r//' check_user_conn.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
+        ...setUpShellScript(MINECLOUD_BASE_DIR, 'get_connection_count.sh', 'minecloud_configs/advanced_configs/get_connection_count.sh'),
+        ...setUpShellScript(MINECLOUD_BASE_DIR, 'check_user_conn.sh', 'server_init_assets/check_user_conn.sh'),
+
         // Setup crontab scheduler, run every 30 min
         InitCommand.shellCommand(
           `(crontab -l 2>/dev/null; echo "*/30 * * * * ${MINECLOUD_BASE_DIR}/check_user_conn.sh") | crontab -`
         )
       ]),
       setupGetLatestBackupScript: new InitConfig([
-        InitFile.fromFileInline(
-          `${MINECLOUD_BASE_DIR}/get_latest_server_backup.sh`,
-          'server_init_assets/get_latest_server_backup.sh'
-        ),
-        InitCommand.shellCommand(`sudo chmod +x get_latest_server_backup.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
-        InitCommand.shellCommand(`sed -i 's/\r//' get_latest_server_backup.sh`, {
-          cwd: MINECLOUD_BASE_DIR
-        }),
+        ...setUpShellScript(MINECLOUD_BASE_DIR, 'get_latest_server_backup.sh', 'server_init_assets/get_latest_server_backup.sh'),
       ]),
       noAction: new InitConfig([])
     }
